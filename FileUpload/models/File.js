c6985057e0ed67db5d266d59686d4ce85a96f50b
HarshalMjn <mahajanh006@gmail.com>
2024-01-18 10:19:33 +0530

    Best Pratice

--- FileUpload/models/File.js
+++ FileUpload/models/File.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const nodemailer = require("nodemailer");
+const  transporter = require("../config/transporter")
 
 const fileSchema = new mongoose.Schema({
     name:{
@@ -25,21 +26,23 @@ fileSchema.post("save", async function(doc) {
         console.log("DOC",doc)
 
         //transporter 
+        const transporterInstance = await transporter();
         //TODO: shift this configuration under /config folder
-        let transporter = nodemailer.createTransport({
-            host: process.env.MAIL_HOST,
-            auth:{
-                user:process.env.MAIL_USER,
-                pass: process.env.MAIL_PASS,
-            },
-        });
+        // let transporter = nodemailer.createTransport({
+        //     host: process.env.MAIL_HOST,
+        //     auth:{
+        //         user:process.env.MAIL_USER,
+        //         pass: process.env.MAIL_PASS,
+        //     },
+        // });
+
 
         //send mail 
-        let info = await transporter.sendMail({
+        let info = await transporterInstance.sendMail({
             from:`CodeHelp - by Harshal`,
             to: doc.email,
             subject: "New File Uploaded on Cloudinary",
-            html:`<h2>Hello Jee</h2> <p>File Uploaded View here: <a href="${doc.imageUrl}">${doc.imageUrl}</a> </p>`,
+            html:`<h2>Hello</h2> <p>File Uploaded View here: <a href="${doc.imageUrl}">${doc.imageUrl}</a> </p>`,
         })
         
         console.log("INFO", info);
